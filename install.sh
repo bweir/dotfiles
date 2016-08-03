ce3fea4d0bf02aebb8e5bf1badee41e87e161f40
Brett Weir <brett@lamestation.com>
2016-08-03 02:41:22 -0700

    Add more programs to install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
+# apt
+
+sudo sed -e 's/^# \(deb .*partner\)/\1/' -i /etc/apt/sources.list
+sudo apt-get update
+
 # base system
 sudo apt-get install \
 	vim tree feh xfburn filezilla \
+	chromium-browser adobe-flashplugin \
 	gimp inkscape imagemagick shutter vlc \
 	qt5-default libqt5serialport5-dev qtbase5-dev-tools qttools5-dev-tools \
 	flex bison help2man \
 	git tiled codeblocks fpc ruby-dev \
+    kicad nodejs npm \
 	pgpgpg libgnome-keyring-dev
 
 # graphics drivers
 # https://help.ubuntu.com/community/BinaryDriverHowto/Nvidia
 #sudo apt-get install nvidia-352
 
-# install jekyll
+# ruby software
 sudo gem install jekyll travis
 
+# nodejs software
+sudo npm install gitbook-cli -g
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
+
+# setup vim
 cp -f .vimrc ~/.vimrc
 
 # install gnome keyring to git
-cd /usr/share/doc/git/contrib/credential/gnome-keyring
-sudo make
-git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
-git config --global push.default simple
+pushd /usr/share/doc/git/contrib/credential/gnome-keyring
+if [ ! -f git-credential-gnome-keyring ] ; then
+    echo "Setting up gnome-keyring for git"
+    sudo make
+    git config --global credential.helper /usr/share/doc/git/contrib/credential/gnome-keyring/git-credential-gnome-keyring
+    git config --global push.default simple
+fi
+popd
 
 #pushd ~
 #mkdir -p Projects
